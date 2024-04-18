c7df14e6dcd11bfae1dc41e8267fa0b455a5c4cf
Oz Tiram <oz.tiram@gmail.com>
2024-04-18 13:55:09 +0200

    Convert Package and PackageSpecifieres to DataModel
    Signed-off-by: Oz Tiram <oz.tiram@gmail.com>

--- src/plette/models/packages.py
+++ src/plette/models/packages.py
@@ -1,7 +1,17 @@
-from .base import DataView
+from .base import DataView, DataModel, DataValidationError
+
+class PackageSpecfiers(DataModel):
+    # TODO: one could add here more validation for path editable
+    # and more stuff which is currently allowed and undocumented
+    __SCHEMA__ = {}
+    __OPTIONAL__ = {
+        "editable": bool,
+        "version": str,
+        "extras": list
+    }
 
 
-class Package(DataView):
+class Package(DataModel):
     """A package requirement specified in a Pipfile.
 
     This is the base class of variants appearing in either `[packages]` or
@@ -11,19 +21,21 @@ class Package(DataView):
     # to have oneof_schema (at least I can't do it), so we wrap this in a
     # top-level key. The Requirement model class implements extra hacks to
     # make this work.
-    __SCHEMA__ = {
-        "__package__": {
-            "oneof_type": ["string", "dict"],
-        },
+    __OPTIONAL__ = {
+        "PackageSpecfiers":  (str, dict)
     }
 
     @classmethod
     def validate(cls, data):
-        # HACK: Make this validatable for Cerberus. See comments in validation
-        # side for more information.
-        super(Package, cls).validate({"__package__": data})
+        if not isinstance(data, (str, dict)):
+            raise DataValidationError(f"invalid type for package: {type(data)}")
+
+        if isinstance(data, str):
+            return
         if isinstance(data, dict):
-            PackageSpecfiers.validate({"__specifiers__": data})
+            PackageSpecfiers.validate(data)
+        else:
+            raise DataValidationError(f"invalid type for package data: {type(data)}")
 
     def __getattr__(self, key):
         if isinstance(self._data, str):
@@ -38,21 +50,9 @@ class Package(DataView):
 
     def __setattr__(self, key, value):
         if key == "_data":
-            super(Package, self).__setattr__(key, value)
+            super().__setattr__(key, value)
         elif key == "version" and isinstance(self._data, str):
             self._data = value
         else:
             self._data[key] = value
 
-class PackageSpecfiers(DataView):
-    # TODO: one could add here more validation for path editable
-    # and more stuff which is currently allowed and undocumented
-    __SCHEMA__ = {
-        "__specifiers__": {
-            "type": "dict",
-            "schema":{
-                "version": {"type": "string"},
-                "extras": {"type": "list"},
-                }
-            }
-        }
